b8bababf81947f876128edc08ea93d60c9dbe5a0
Megumi Kimura <kimuramegumi@gmail.com>
2023-01-25 22:20:15 +0100

    update e2e test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,6 @@
 const EMAIL = Cypress.env("USER_EMAIL");
 const PASSWORD = Cypress.env("USER_PASSWORD");
-const TOKEN = Cypress.env("USER_TOKEN");
 const USER_NAME = Cypress.env("USER_NAME");
-// const accessToken =
-//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywibmFtZSI6Ik1lZ3VtaSIsImVtYWlsIjoibWVndW1pQG5vcm9mZi5ubyIsImF2YXRhciI6Imh0dHBzOi8vY2RuLnBpeGFiYXkuY29tL3Bob3RvLzIwMjAvMDYvMjYvMDMvNTQveW91bmctNTM0MTU3N185NjBfNzIwLmpwZyIsImJhbm5lciI6Imh0dHBzOi8vY2RuLnBpeGFiYXkuY29tL3Bob3RvLzIwMTgvMTAvMTMvMTcvMzEvbGVhdmVzLTM3NDQ2NDlfOTYwXzcyMC5qcGciLCJpYXQiOjE2NzQ2NDY0NjF9.Hm8T9Ojgo2efGpUTTm29FLHkzFb91hk--YHPIJfgfXs";
 
 describe("login", () => {
   it("saves access token in local storage and redirect the user to profile page", () => {
@@ -30,8 +27,6 @@ describe("login", () => {
         expect(localStorage.getItem("token")).to.exist;
       });
 
-    cy.location().should((loc) => {
-      expect(loc.search).to.eq(`?view=profile&name=${USER_NAME}`);
-    });
+    cy.location("search").should("eq", `?view=profile&name=${USER_NAME}`);
   });
 });
--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -1,28 +1,27 @@
-const EMAIL = Cypress.env("EMAIL");
-const PASSWORD = Cypress.env("PASSWORD");
-const TOKEN = Cypress.env("TOKEN");
-const accessToken =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywibmFtZSI6Ik1lZ3VtaSIsImVtYWlsIjoibWVndW1pQG5vcm9mZi5ubyIsImF2YXRhciI6Imh0dHBzOi8vY2RuLnBpeGFiYXkuY29tL3Bob3RvLzIwMjAvMDYvMjYvMDMvNTQveW91bmctNTM0MTU3N185NjBfNzIwLmpwZyIsImJhbm5lciI6Imh0dHBzOi8vY2RuLnBpeGFiYXkuY29tL3Bob3RvLzIwMTgvMTAvMTMvMTcvMzEvbGVhdmVzLTM3NDQ2NDlfOTYwXzcyMC5qcGciLCJpYXQiOjE2NzQ2NDY0NjF9.Hm8T9Ojgo2efGpUTTm29FLHkzFb91hk--YHPIJfgfXs";
+const EMAIL = Cypress.env("USER_EMAIL");
+const PASSWORD = Cypress.env("USER_PASSWORD");
+const TOKEN = Cypress.env("USER_TOKEN");
+const USER_NAME = Cypress.env("USER_NAME");
+
 const profile = {
-  name: "Megumi",
-  email: "megumi@noroff.no",
+  name: USER_NAME,
+  email: EMAIL,
 };
 
 describe("logout", () => {
   it("delete access token after successful logout", () => {
-    // cy.clearLocalStorage();
-    localStorage.setItem("token", JSON.stringify(accessToken));
+    localStorage.setItem("token", JSON.stringify(TOKEN));
     localStorage.setItem("profile", JSON.stringify(profile));
     cy.wait(1000);
 
-    cy.visit("/?view=profile&name=Megumi");
+    cy.visit(`/?view=profile&name=${USER_NAME}`);
     cy.wait(1000);
     cy.get("button[data-auth='logout']")
       .click()
       .then(() => {
-        // expect(cy.location("/"));
-
         expect(localStorage.getItem("token")).to.not.exist;
+        expect(localStorage.getItem("profile")).to.not.exist;
       });
+    cy.location("pathname").should("eq", "/");
   });
 });
